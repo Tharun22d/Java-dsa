8dce453282d84d0d4101a91150e237a7dffa98e2
Tharun22d <mrphysics2022d@gmail.com>
2025-06-14 12:58:09 +0530

    day : 3 Arrays & ArrayList

---
+++ DSA/1. Arrays/src/leetcode/LeetCode4.java
@@ -0,0 +1,50 @@
+/*
+1672. Richest Customer Wealth
+
+ques : in a 2d array compute the sum of each subarray and return the maximum value
+
+Input: accounts = [[1,5],[7,3],[3,5]]
+Output: 10
+Explanation:
+1st customer has wealth = 6
+2nd customer has wealth = 10
+3rd customer has wealth = 8
+The 2nd customer is the richest with a wealth of 10.  -- ouput
+ */
+
+package leetcode;
+import java.util.*;
+
+public class LeetCode4 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the no of rows : ");
+        int row = input.nextInt();
+        System.out.println("enter the no of columns : ");
+        int col = input.nextInt();
+        int[][] arr = new int[row][col];
+
+        for(int i = 0 ; i < arr.length; i++) {
+            for(int j = 0 ; j < arr[i].length ;j++){
+                arr[i][j]  = input.nextInt();
+            }
+        }
+        int res = maximumWealth(arr);
+        System.out.print(res);
+    }
+    public static int maximumWealth(int[][] accounts) {
+
+        int max = 0;
+        for(int i = 0 ; i <accounts.length;i++){
+            int sum = 0 ;  //since sum updates for each itreation of i so inside the loop
+            for(int j = 0 ; j<accounts[i].length; j++){
+                sum = sum + accounts[i][j];
+            }
+            if(sum>max){
+                max = sum;
+            }
+
+        }
+        return max;
+    }
+}
---
+++ DSA/1. Arrays/src/leetcode/LeetCode5.java
@@ -0,0 +1,38 @@
+/*
+1470. Shuffle the Array
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+Input: nums = [1,2,3,4,4,3,2,1], n = 4
+Output: [1,4,2,3,3,2,4,1]
+ */
+
+package leetcode;
+import java.util.*;
+
+public class LeetCode5 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        System.out.print("Enter the array values : ");
+        for(int i = 0; i < arr.length ; i++){
+            arr[i] = input.nextInt();
+        }
+        System.out.print("Enter the index value : ");
+        int index = input.nextInt();
+        int[] res = shuffle(arr,index);
+        System.out.println(Arrays.toString(res));
+    }
+    public static int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+
+        for(int i = 0 , j=0; i<nums.length ; i=i+2, j++){
+           ans[i] = nums[j];        //ans[0] = nums[0]             //ans[2]  = nums[1] (nums incremented by 1 wehere as ans incremented by 2)
+           ans[i+1] = nums[n+j];    //ans[1] = nums[3+0] = nums[3]  // ans[3] = nums[3+1]
+        }
+
+        return ans;
+    }
+}
