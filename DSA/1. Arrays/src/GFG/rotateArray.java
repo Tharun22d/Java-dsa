612851ba5e11afb6eb184657d7fb88345493c2f4
Tharun22d <mrphysics2022d@gmail.com>
2025-06-17 21:50:51 +0530

    day 5 arrays/arraylist

---
+++ DSA/1. Arrays/src/GFG/rotateArray.java
@@ -0,0 +1,46 @@
+//rotate an array towards left
+//apprach - reverse the first half till the k , reverse the next half , revesse the whole arrau
+//apporach for right rotation - reverse the last till the k values , then the remining first half , then the whole array
+
+package GFG;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class rotateArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = {1,2,3,4,5};
+        int d = 2;
+        int[] res = rotateArr(arr,d);
+        System.out.println(Arrays.toString(res));
+    }
+    static int[] rotateArr(int arr[], int d) {
+        // add your code here
+        //reverse the first half till d
+        if(arr.length ==0 || d ==0){
+            return arr;
+        }
+        d = d%arr.length;
+        for(int i = 0, j = d-1;i<j; i++,j--){
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+        //reverse the 2nd half d and >than d
+        for(int i = d, j = arr.length-1 ; i<j ;i++,j--){
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+
+        //reverse the entire array
+        for(int i = 0 , j = arr.length-1 ; i<j ;i++,j--){
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+
+        return arr;
+    }
+}
